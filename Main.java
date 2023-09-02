65e747e5e3b08da4006769454c6012c81f7cb352
Mahmoud Ahmed <buttererror@gmail.com>
2023-09-02 05:34:07 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+//         System.out.println("Hello world");
+//         System.out.println("I am learning Java at 4 AM");
+//         System.out.println(5 + 5);
+//         System.out.println(10 * 10);
+        String name = "Mahmoud";
+        int x = 5;
+        char character = 'a';
+        float y = 5.5f;
+        boolean b = true;
+        System.out.println(name);
+        System.out.println(x);
+        System.out.println(character);
+        System.out.println(y);
+        System.out.println(b);
+    }
+}
